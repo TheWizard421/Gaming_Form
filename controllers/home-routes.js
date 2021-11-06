9c6ba7edcb24828cdee173073eeec927c410db95
ForeytKL22 <krforeyt@gmail.com>
2021-11-06 12:35:35 -0600

    signup route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,17 @@
 const router = require('express').Router();
 
+
 router.get('/', (req, res) => {
-  res.render('homepage');
+  console.log(req.body);
+  res.render('homepage', {
+    loggedIn: req.session.loggedIn
+  });
 });
+
+
+router.get('/signup', (req, res) => {
+  res.render('signup');
+})
+
 
 module.exports = router;
