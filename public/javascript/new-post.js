62652c26e421abe2c533124206433003b76c2170
ForeytKL22 <krforeyt@gmail.com>
2021-11-08 18:56:40 -0700

    added filepond

--- public/javascript/new-post.js
+++ public/javascript/new-post.js
@@ -22,27 +22,3 @@ async function newPost(event) {
 }
 
 document.querySelector('.new-post-form').addEventListener('submit', newPost);
-
-
-
-  const post_title = document.querySelector('input[name="post-title"]').value;
-  const post_body = document.querySelector('textarea[name="post-body"]').value;
-
-  const response = await fetch(`/api/posts`, {
-    method: "POST",
-    body: JSON.stringify({
-      post_title,
-      post_body,
-    }),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  if (response.ok) {
-    document.location.replace("/profile");
-  } else {
-    alert(response.statusText);
-  }
-}
-
-document.querySelector(".new-post-form").addEventListener("submit", newPost);
