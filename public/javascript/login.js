59336ff834a96299e38700c14fde26fec8da209e
Anthony Gonzalez <shibby75@live.com>
2021-11-08 18:43:57 -0700

    fixed errors with editing posts, posting new comments and date formatting

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,59 +1,55 @@
 async function signupForm(event) {
   event.preventDefault();
 
-  const username = document.querySelector('#username-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
+  const username = document.querySelector("#username-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
 
   if (username && password && email) {
-      await fetch('/api/users', {
-          method: 'post',
-          body: JSON.stringify({
-              username,
-              password,
-              email
-          }),
-          headers: { 'Content-Type': 'application/json' }
-      })
-      if (response.ok) {
-          console.log('success');
-          document.location.replace('/profile')
-      } else {
-          alert(response.statusText);
-      }
-  }
-}
-
-
-async function loginForm(event) {
-  event.preventDefault();
-
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
-
-  if (email && password) {
-    const response = await fetch('/api/users/login', {
-      method: 'post',
+    const response = await fetch("/api/users", {
+      method: "post",
       body: JSON.stringify({
+        username,
+        password,
         email,
-        password
       }),
-      headers: { 'Content-Type': 'application/json' }
+      headers: { "Content-Type": "application/json" },
     });
-
     if (response.ok) {
-      document.location.replace('/profile');
+      console.log("success");
+      document.location.replace("/profile");
     } else {
       alert(response.statusText);
     }
   }
 }
 
-document.querySelector('.login-form').addEventListener('submit', loginForm);
-document.querySelector('.signup-form').addEventListener('submit', signupForm);
+async function loginForm(event) {
+  event.preventDefault();
 
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
 
+  if (email && password) {
+    const response = await fetch("/api/users/login", {
+      method: "post",
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
 
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+document.querySelector(".login-form").addEventListener("submit", loginForm);
+document.querySelector(".signup-form").addEventListener("submit", signupForm);
 
 // async function loginForm(event) {
 //     event.preventDefault();
@@ -77,13 +73,12 @@ document.querySelector('.signup-form').addEventListener('submit', signupForm);
 //     }
 // }
 
-
 // async function signupForm(event) {
 //     event.preventDefault();
 //     const username = document.querySelector('#username-signup').value.trim();
 //     const email = document.querySelector('#email-signup').value.trim();
 //     const password = document.querySelector('#password-signup').value.trim();
-  
+
 //     if (username && email && password) {
 //       const response = await fetch('/api/users', {
 //         method: 'post',
@@ -101,7 +96,6 @@ document.querySelector('.signup-form').addEventListener('submit', signupForm);
 //       }
 //     }
 //   }
-
 
 // document.querySelector('.login-form').addEventListener('submit', loginForm);
 // document.querySelector('.signup-form').addEventListener('submit', signupForm);
